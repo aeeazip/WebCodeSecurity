ea88eac829026346b6513f54632849b0e8f8f0f1
aeeazip <aeea0520@gmail.com>
2024-06-07 03:07:02 +0900

    feat : 13주차 과제 업로드

---
+++ Practice13/W13_01/Practice13_01.java
@@ -0,0 +1,44 @@
+package Practice13.W13_01;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class Practice13_01 {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
+        /*
+         * 필요한 정보
+         *
+         * 1. 클래스 이름
+         * 2. 소속 변수의 이름과 타입, 접근 제어자
+         * 3. 소속 메소드의 메소드 시그니처
+         * 4. 생성자 시그니처
+         */
+
+        Class<?> cls = Class.forName("Date");
+
+        // 메소드 정보 얻기
+        Method[] mlist = cls.getDeclaredMethods();
+        for(Method m: mlist) {
+            System.out.println("클래스명 : " + m.getDeclaringClass()); // 1. 클래스 이름
+            System.out.println("소속 메소드 시그니처 : " + m.toString()); // 3. 메소드 시그니처
+        }
+
+        // 필드 정보 얻기
+        Field[] flist = cls.getDeclaredFields();
+        for(Field f : flist) {
+            System.out.println(
+                    "클래스 필드명 : " + f.getName() +
+                    " 클래스 필드 타입 : " + f.getType() +
+                    " 클래스 필드 접근제어자 : " + Modifier.toString(f.getModifiers())
+            ); // 2. 소속 변수의 이름과 타입, 접근 제어자
+        }
+
+        // 생성자 정보 얻기
+        Constructor<?>[] clist = cls.getDeclaredConstructors();
+        for(Constructor<?> c : clist) {
+            System.out.println("생성자 시그니처 : " + c.toString()); // 4. 생성자 시그니처
+        }
+    }
+}
---
+++ Practice13/W13_02/Practice13_02.java
@@ -0,0 +1,23 @@
+package Practice13.W13_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+
+public class Practice13_02 {
+    public static void main() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("날짜를 입력하세요 : ");
+        String[] numbers = br.readLine().split("-");
+
+        int year = Integer.parseInt(numbers[0]);
+        int month = Integer.parseInt(numbers[1]);
+        int day = Integer.parseInt(numbers[2]);
+//        Date date = new Date(year, month, day);
+//
+//        String result = date.printWestern();
+//        Systemstem.out.println("서양식 표기 : " + result);
+    }
+}
